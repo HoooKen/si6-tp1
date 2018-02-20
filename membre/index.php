95a60b289ea93d0abb203b3897810e65e0f1d1ba
HoooKen <36658957+hoooken@users.noreply.github.com>
2018-02-20 11:45:03 +0100

    [+] Ajout du design du panel membre

--- membre/index.php
+++ membre/index.php
@@ -1,21 +1,32 @@
 <html>
-    <head>
-        <link rel="stylesheet" href="design.css" />
-        <title>Panel Membre</title>
-    </head>
-    <body>
-        <?php
-        require_once('../inc/header.php');
-            if(!$_SESSION['pseudo'] == NULL)
-            {
-                ?>
-            <div class="panel">
-                
-            </div>
-        <?php
-            }
-            require_once('../inc/footer.php');
-        ?>
-    </body>
+<head>
+    <link rel="stylesheet" href="design.css" />
+    <title>Panel Membre</title>
+</head>
+<body>
+<?php
+require_once('../inc/header.php');
+$connected=true;
+if($connected == true)
+{
+    ?>
+    <br />
+    <br />
+
+    <div class="link">
+        <a href="#aperçu">Aperçu du compte</a>
+        <a href="#modif">Modifier le profil</a>
+    </div>
+    <br />
+    <br/>
+    <br/>
+    <br />
+    <br />
+    <br />
+    <?php
+}
+require_once('../inc/footer.php');
+?>
+</body>
 
 </html>
