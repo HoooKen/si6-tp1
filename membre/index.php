0be2cb5ab6d08d2579cc9cca244ee3c8f3a8aac5
HoooKen <36658957+hoooken@users.noreply.github.com>
2018-02-20 12:05:08 +0100

    [+] Correction du panel membre

--- membre/index.php
+++ membre/index.php
@@ -14,8 +14,21 @@ if($connected == true)
     <br />
 
     <div class="link">
-        <a href="#aperçu">Aperçu du compte</a>
-        <a href="#modif">Modifier le profil</a>
+        <ul>
+            <li><a href="#aperçu">Aperçu du compte</a></li>
+            </br>
+            <li><a href="#modif" style=padding-right:287px;>Modifier le profil</a></li>
+            <br />
+            <li><a href="#mdp" style="padding-right:216px;">Changer de mot de passe</a></li>
+            <br />
+            <li><a href="#mdp" style="padding-right:280px;">Modifier la société</a></li>
+            <br />
+            <li><a href="#mdp" style="padding-right:204px;">Abonnemment et facturation</a></li>
+            <br />
+            <li><a href="#mdp" style="padding-right:212px;">Gestion des utilisateurs</a></li>
+
+
+        </ul>
     </div>
     <br />
     <br/>
@@ -23,6 +36,11 @@ if($connected == true)
     <br />
     <br />
     <br />
+    <br />    <br />    <br />    <br />    <br />
+
+
+
+
     <?php
 }
 require_once('../inc/footer.php');
